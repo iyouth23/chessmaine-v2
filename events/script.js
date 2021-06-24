6f3fde2858fe083719344ed8ce0afbdcf88afb3b
Ian Youth <ian.youth@wustl.edu>
2021-06-24 13:52:31 -0400

    Updated Events Google Calendar; Fixed folder layout.

--- events/script.js
+++
@@ -1,6 +0,0 @@
-var upcomingEventsButton = document.getElementsByClassName("upcoming-events")[0];
-upcomingEventsButton.addEventListener("click", goToEventsPage);
-
-function goToEventsPage() {
-    location.href = "../navbar-pages/events.html";
-}
